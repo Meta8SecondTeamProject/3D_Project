fbb6e3fa4c43b91a742a1b45d33d96fcbfc3cfce
7220RR <sjm25148@gmail.com>
2024-11-27 13:10:25 +0900

    짜투리 스크립트들 생성 내용X

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,19 +5,22 @@ using UnityEngine;
 public class GameManager : SingletonManager<GameManager>
 {
 
-	internal Player player;
-	internal List<Enemy> flies = new List<Enemy>();
+    internal Player player;
+    internal List<Enemy> flies = new List<Enemy>();
+    public int money = 0;
+    public int ammo = 4;
+    public int maxAmmon = 16;
 
-	protected override void Awake()
-	{
-		base.Awake();
-		player = FindAnyObjectByType<Player>();
-	}
+    protected override void Awake()
+    {
+        base.Awake();
+        player = FindAnyObjectByType<Player>();
+    }
 
-	private void Start()
-	{
+    private void Start()
+    {
 
-	}
+    }
 
 
 }
---
+++ Assets/Scripts/JDY/Doctor_Frog_Test.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doctor_Frog_Test : NPC_Test
+{
+    public override void Interaction()
+    {
+        print("닥터 개구리와 상호작용!");
+        if (gameManager.money >= 5 /*&& gameManager.player.health*/)
+        {
+            //gameManager.player.health += 1;
+            gameManager.money -= 5;
+        }
+    }
+}
