201a2fe4c85872a3ac38caa20ba191f29558727e
Chanyoung <qpalzm60515@gmail.com>
2024-11-27 16:54:29 +0900

    릴리패드 조정/파리사인코사인경로설정/스포너조정/로테이트타겟스크립트조정

--- Assets/Scripts/KCY/Flies.cs
+++ Assets/Scripts/KCY/Flies.cs
@@ -4,19 +4,42 @@ using UnityEngine;
 
 public class Flies : MonoBehaviour
 {
-	public Transform fliesPos;
-	public Transform moveMentPos;
-	public float rotateSpeed;
-	private Rigidbody rb;
-
+	public RotateTarget rotateTarget;
+	private Flies flies;
+	private FliesMovement fliesMovement;
+	public Mesh mesh;
+	private void Awake()
+	{
+		flies = GetComponentInChildren<Flies>();
+		fliesMovement = GetComponentInChildren<FliesMovement>();
+	}
 	private void Start()
 	{
-		rb = GetComponentInChildren<Rigidbody>();
+		//if (fliesMovement.rand == 0)
+		//{
+		//	rotateTarget.transform.position = new Vector3(rotateTarget.transform.position.x, rotateTarget.transform.position.y + 9f, rotateTarget.transform.position.z);
+		//}
+		//else
+		//{
+		//	rotateTarget.transform.position = new Vector3(rotateTarget.transform.position.x, rotateTarget.transform.position.y - 9f, rotateTarget.transform.position.z);
+		//}
 	}
 
 	private void Update()
 	{
-		//fliesPos.RotateAround(moveMentPos.position, Vector3.up, rotateSpeed);
-		//rb.position.
+
+	}
+	private void OnDrawGizmos()
+	{
+		//if (fliesMovement.rand == 0)
+		//{
+		//	Gizmos.color = Color.yellow;
+		//}
+		//else
+		//{
+		//	Gizmos.color = Color.blue;
+		//}
+		//Gizmos.DrawWireMesh(mesh, rotateTarget.transform.position, Quaternion.identity, new Vector3(35, 9, 35));
+
 	}
 }
