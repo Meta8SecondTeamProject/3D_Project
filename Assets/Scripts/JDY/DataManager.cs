082dbdb1f1fc5630187f459b18fecbdcea0450f2
7220RR <sjm25148@gmail.com>
2024-12-02 13:09:20 +0900

    UI관련 수정중

--- Assets/Scripts/JDY/DataManager.cs
+++ Assets/Scripts/JDY/DataManager.cs
@@ -5,30 +5,30 @@ using UnityEngine;
 public class DataManager : SingletonManager<DataManager>
 {
 
-	public int HP { get { return currentHP; } set { currentHP = Mathf.Clamp(value, 0, 2); } }
-	public int currentHP;
-	public int ammo { get { return currentAmmo; } set { currentAmmo = Mathf.Clamp(value, 0, maxAmmo); } }
+    public int HP { get { return currentHP; } set { currentHP = Mathf.Clamp(value, 0, 2); } }
+    public int currentHP;
+    public int ammo { get { return currentAmmo; } set { currentAmmo = Mathf.Clamp(value, 0, maxAmmo); } }
 
-	private int currentAmmo;
-	public int maxAmmo;
-	public int money;
-	public int jumpCount;
-	public bool isHat;
-	public bool isAmmoBelt;
-	public bool isDoubleJump;
-	public int triggerOn;
+    private int currentAmmo;
+    public int maxAmmo;
+    public int money;
+    public int jumpCount;
+    public bool isHat;
+    public bool isAmmoBelt;
+    public bool isDoubleJump;
+    public int triggerOn;
 
 
-	public float bombFliesSpeed;
-	public float fishSpeed;
-	public float birdSpeed;
+    public float bombFliesSpeed;
+    public float fishSpeed;
+    public float birdSpeed;
 
-	public int fliesMaxCount;
-	public int fishMaxCount;
-	public int birdMaxCount;
-	public int birdBlackMaxCount;
+    public int fliesMaxCount;
+    public int fishMaxCount;
+    public int birdMaxCount;
+    public int birdBlackMaxCount;
 
-	public int fliesKillCount;
-	public int fishKillCount;
-	public int birdKillCount;
+    public int fliesKillCount;
+    public int fishKillCount;
+    public int birdKillCount;
 }
