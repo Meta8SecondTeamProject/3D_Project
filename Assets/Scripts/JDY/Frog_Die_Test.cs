c4376407c20d94a8987688d891d67a2eafde5498
JDY_NOTE\jdy00 <sjm25148@gmail.com>
2024-12-02 09:26:08 +0900

    뭐했지... 진짜모름

---
+++ Assets/Scripts/JDY/Frog_Die_Test.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Frog_Die_Test : MonoBehaviour
+{
+    public Rigidbody[] rigidbodys;
+    public float explosionForce = 5000f;
+    public float explosionRadius = 5f;
+    public Vector3 explosionOffset = Vector3.up;
+
+    public float delay = 5f;
+
+    [ContextMenu("Test/사지분해")]
+    public void TriggerDeathEffect()
+    {
+        Vector3 explosionCenter = transform.position + explosionOffset;
+
+        foreach (Rigidbody rigidbody in rigidbodys)
+        {
+            if(rigidbody == null) continue;
+
+            rigidbody.isKinematic = false;
+
+            rigidbody.useGravity = true;
+
+            rigidbody.AddExplosionForce(explosionForce, explosionCenter, explosionRadius);
+
+            StartCoroutine(DisableObject(rigidbody.gameObject));
+        }
+    }
+
+    private IEnumerator DisableObject(GameObject obj)
+    {
+        yield return new WaitForSeconds(delay);
+        obj.SetActive(false);
+    }
+}
