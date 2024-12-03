6cca459af8f98115d8df464571314fdbb416327b
HundredBong <kag2479@naver.com>
2024-12-03 17:02:53 +0900

    LilyPad 주석 추가

--- Assets/Scripts/Frog/LilyPad.cs
+++ Assets/Scripts/Frog/LilyPad.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class LilyPad : MonoBehaviour
 {
-    [SerializeField] private float forwardForce = 10f;
-    [SerializeField] private float upForce = 40f;
+    [SerializeField][Header("전방 방향으로 줄 힘")] private float forwardForce = 10f;
+    [SerializeField][Header("위쪽 방향으로 줄 힘")] private float upForce = 40f;
 
     private void OnCollisionEnter(Collision other)
     {
@@ -17,8 +17,10 @@ public class LilyPad : MonoBehaviour
 
             if (other.gameObject.TryGetComponent(out Rigidbody rb))
             {
+                //원래 방향 벡터는 transform.forward * vertical + transform.right * horizontal이지만, 
+                //우리 방향 벡터는 개구리의 Rotation이 정상적이이 않으므로 transform.right * -horizontal로 계산함
                 //rb.velocity=new Vector3(rb.velocity.x,0,rb.velocity.z);
-                rb.velocity = new Vector3(rb.velocity.x, upForce, rb.velocity.z) + (rb.transform.right * forwardForce);
+                rb.velocity = new Vector3(rb.velocity.x, upForce, rb.velocity.z) + (rb.transform.right * -forwardForce);
                 //rb.AddForce(rb.transform.up * upForce, ForceMode.VelocityChange);
             }
         }
