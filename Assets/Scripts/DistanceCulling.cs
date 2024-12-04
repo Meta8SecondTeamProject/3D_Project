390ccd22d75d9c6083ddb447fc110045525e4614
HundredBong <kag2479@naver.com>
2024-12-04 14:37:22 +0900

    팀원들의 강력한 요구로 Distance Culling Singleton 패턴으로 리팩토링

--- Assets/Scripts/DistanceCulling.cs
+++ Assets/Scripts/DistanceCulling.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DistanceCulling : MonoBehaviour
+public class DistanceCulling : SingletonManager<DistanceCulling>
 {
     [SerializeField, Header("컬링할 거리")]
-    private float cullingDis = 100f;
+    private float cullingDis = 400f;
 
     private Transform player;
     //컬링할 오브젝트를 리스트로 관리, 태그로는 못함
@@ -13,7 +13,8 @@ public class DistanceCulling : MonoBehaviour
     public string explanation = "컬링할 오브젝트 태그를 Culling으로 바꿔주세요.\n눈에 띄지않는 작은 오브젝트가 적합합니다.";
     public List<GameObject> cullingObj = new List<GameObject>();
 
-    private void Start()
+    [ContextMenu("Set")]
+    public void SetPlayerAndTarget()
     {
         //플레이어를 매번 생성하는 로직이므로 이렇게 초기화안하면 null뜰거같음
         //아마? 뜰?거임
@@ -37,23 +38,30 @@ public class DistanceCulling : MonoBehaviour
         }
     }
 
+    [ContextMenu("Reset")]
+    public void ResetPlayerAndTarget()
+    {
+        player = null;
+        cullingObj.Clear();
+    }
+    
     //플레이어와 Culling 태그가 붙은 오브젝트를 찾아와서 플레이어와의 거리에 따라 비활성화 처리함
     private void Update()
     {
         //null 체크
         if (player == null)
             return;
-
+        
         //태그로 찾는건 좋으나, 매 프레임마다 Find를 호출하는건 성능에 좋지도 않고,
         //Find는 비활성화된 오브젝트를 찾지 못하므로 다시 활성화되게 하려면 다른 로직이 필요함
         foreach (GameObject obj in cullingObj)//GameObject.FindGameObjectsWithTag("Culling"))
         {
             float distance = Vector3.Distance(player.transform.position, obj.transform.position);
             Debug.Log($"거리 : {distance}");
+            
             Debug.Log($"플레이어 이름 : {player.name}");
             Debug.Log($"오브젝트 이름 : {obj.name}");
             obj.SetActive(distance <= cullingDis);                
         }
     }
 }
-
