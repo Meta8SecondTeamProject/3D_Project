893e265a11c07bbd0f3329e07419a47f69906da7
DESKTOP-99CV4K8\qpalz <qpalzm60515@gmail.com>
2024-12-05 22:24:28 +0900

    EnemySpawner 스크립트 병합 완료(찬영,봉훈)

--- Assets/Scripts/KCY/Enemy/EnemySpawner.cs
+++ Assets/Scripts/KCY/Enemy/EnemySpawner.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UniRan = UnityEngine.Random;
 
 public class EnemySpawner : MonoBehaviour
@@ -11,6 +12,10 @@ public class EnemySpawner : MonoBehaviour
 	private BoxCollider rangeColl;
 	private ObjectPool enemyPool;
 	private float bomb;
+
+	[Header("Bird, Fish, Toad")]
+	public GameObject[] bossEnemy;
+
 	private void Awake()
 	{
 		enemyPool = FindAnyObjectByType<ObjectPool>();
@@ -24,10 +29,6 @@ public class EnemySpawner : MonoBehaviour
 				gameObject.SetActive(true);
 				break;
 		}
-	}
-	private void OnEnable()
-	{
-
 	}
 
 	private void Start()
@@ -56,11 +57,17 @@ public class EnemySpawner : MonoBehaviour
 	{
 		while (true)
 		{
+			Debug.Log($"최대 Fish 스폰 카운트 : {DataManager.Instance.fishMaxSpawnCount}");
+			Debug.Log($"비교 연산자 체크용 : {GameManager.Instance.enemy[numberOfEnemy].Count} < {enemyMaxCount}");
+			//Debug.Log($"활성화된 트리거 수 : {DataManager.Instance.triggerOn}");
 			if (numberOfEnemy == 0)
 			{
 				bomb = UniRan.Range(0f, 1f);
 				if (bomb > 0.9f) numberOfEnemy = 3;
 			}
+			// TODO : 추후 주석 해제해서 트리거 3개 이상 활성화되면 Enemy 스폰되게,
+			//if (DataManager.Instance.triggerOn >= 3)
+			//{
 			enemyPool.Pop(enemyPool.obj[numberOfEnemy].name);
 			enemyPool.obj[numberOfEnemy].transform.position = GetSpawnPos();
 			if (numberOfEnemy == 3) numberOfEnemy = 0;
@@ -69,11 +76,43 @@ public class EnemySpawner : MonoBehaviour
 
 			Debug.Log(GameManager.Instance.enemy[numberOfEnemy].Count);
 			yield return new WaitUntil(() => GameManager.Instance.enemy[numberOfEnemy].Count < enemyMaxCount);
+			//TODO : 목표 Enemy량을 채우면 보스 Enemy가 스폰되도록 설정,
+			//       지금 쌉하드코딩인데 이거 고칠방법 아는사람 손,
+			//       인덱스를 쓰던 뭘 하던 하긴 해야하는데 으으윽흑흑
+			//if(DataManager.Instance.birdMaxCount > DataManager.Instance.birdKillCount)
+			//{
+			//	SpawnBoss();
+			//}
+			//}
 		}
 	}
 
 
+	private void SpawnBoss()
+	{
+		//TODO : SapwnBoss로직 하드코딩, 상의후 결정
+		int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+		GameObject boss;
 
+		switch (sceneIndex)
+		{
+			case 3: //BBH_Scene
+				boss = Instantiate(bossEnemy[0]);
+				boss.transform.position = GetSpawnPos();
+				boss.transform.rotation = Quaternion.identity;
+				break;
+			case 4: //JDY_Scene
+				boss = Instantiate(bossEnemy[0]);
+				boss.transform.position = GetSpawnPos();
+				boss.transform.rotation = Quaternion.identity;
+				break;
+			default:
+				break;
+		}
+		//TODO : 보스 잡으면 포탈 활성화되도록 bool변수던 뭐던 추가하기, 다음 씬에도 연동되야하니 DataManager에 추가
+		// 
+
+	}
 	private Vector3 GetSpawnPos()
 	{
 		Vector3 originPos = transform.position;
