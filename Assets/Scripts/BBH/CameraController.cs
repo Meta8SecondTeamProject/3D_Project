5d5767c2a30dcd2d2244585f4e77f98ecc6b372d
HundredBong <kag2479@naver.com>
2024-12-05 09:30:13 +0900

    테스트씬 수정 및 프리팹 추가

--- Assets/Scripts/BBH/CameraController.cs
+++ Assets/Scripts/BBH/CameraController.cs
@@ -15,8 +15,8 @@ public class CameraController : MonoBehaviour
 
     private void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+        //Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.visible = false;
     }
 
     private void Update()
