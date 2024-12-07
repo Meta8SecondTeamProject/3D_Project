5ace1312244bcb0d63c3aac7de60b7bfe2855170
DESKTOP-99CV4K8\qpalz <qpalzm60515@gmail.com>
2024-12-08 00:54:27 +0900

    개구리 죽을 때 재시작안되는 버그 수정중 / 씬 로딩될 때 플레이어 안생기는 버그 수정중

--- Assets/Scripts/JDY/Test/Frog_Die_Test.cs
+++ Assets/Scripts/JDY/Test/Frog_Die_Test.cs
@@ -3,79 +3,100 @@ using UnityEngine;
 
 public class Frog_Die_Test : MonoBehaviour
 {
-    private Vector3[] pos;
-    public Rigidbody[] rigidbodys;
-    public float explosionForce = 5000f;
-    public float explosionRadius = 5f;
-    public Vector3 explosionOffset = Vector3.up;
+	private Vector3[] pos;
+	public Rigidbody[] rigidbodys;
+	public float explosionForce = 5000f;
+	public float explosionRadius = 5f;
+	public Vector3 explosionOffset = Vector3.up;
 
-    public float delay = 5f;
+	public float delay = 5f;
 
-    private int remainingObjects;
+	private int remainingObjects;
 
-    private void OnEnable()
-    {
-        pos = new Vector3[rigidbodys.Length];
-        if (rigidbodys.Length != pos.Length)
-        {
-            Debug.LogError("Frog_Die_Test / OnEnable");
-            return;
-        }
-        for (int i = 0; i < rigidbodys.Length; i++)
-        {
-            pos[i] = rigidbodys[i].position;
-        }
-    }
+	private void OnEnable()
+	{
+		if (rigidbodys == null || rigidbodys.Length == 0)
+		{
+			Debug.LogError("Frog_Die_Test / OnEnable: No rigidbodies assigned.");
+			return;
+		}
+		pos = new Vector3[rigidbodys.Length];
+		if (rigidbodys.Length != pos.Length)
+		{
+			Debug.LogError("Frog_Die_Test / OnEnable");
+			return;
+		}
+		for (int i = 0; i < rigidbodys.Length; i++)
+		{
+			pos[i] = rigidbodys[i].position;
+		}
+	}
 
-    private void OnDisable()
-    {
-        if (rigidbodys.Length != pos.Length)
-        {
-            Debug.LogError("Frog_Die_Test / OnDisable");
-        }
-        for (int i = 0; i < rigidbodys.Length; i++)
-        {
-            rigidbodys[i].position = pos[i];
-        }
-    }
+	private void OnDisable()
+	{
+		if (rigidbodys.Length != pos.Length)
+		{
+			Debug.LogError("Frog_Die_Test / OnDisable: Rigidbodies and positions mismatch.");
+		}
+		for (int i = 0; i < rigidbodys.Length; i++)
+		{
+			rigidbodys[i].position = pos[i];
 
-    private void Start()
-    {
-        TriggerDeathEffect();
-    }
+		}
+	}
 
-
-    [ContextMenu("Test/사지분해")]
-    public void TriggerDeathEffect()
-    {
-        remainingObjects = rigidbodys.Length;
-
-        Vector3 explosionCenter = transform.position + explosionOffset;
-
-        foreach (Rigidbody rigidbody in rigidbodys)
-        {
-            if (rigidbody == null) continue;
-
-            rigidbody.isKinematic = false;
-
-            rigidbody.useGravity = true;
-
-            rigidbody.AddExplosionForce(explosionForce, explosionCenter, explosionRadius);
-
-            StartCoroutine(DisableObject(rigidbody.gameObject));
-        }
+	private void Start()
+	{
+		TriggerDeathEffect();
+	}
 
 
-    }
+	[ContextMenu("Test/사지분해")]
+	public void TriggerDeathEffect()
+	{
+		if (rigidbodys == null || rigidbodys.Length == 0)
+		{
+			Debug.LogError("Frog_Die_Test / TriggerDeathEffect: No rigidbodies assigned.");
+			return;
+		}
 
-    private IEnumerator DisableObject(GameObject obj)
-    {
-        yield return new WaitForSeconds(delay);
-        obj.SetActive(false);
-        remainingObjects--;
-        if (remainingObjects <= 0 && CompareTag("Frog"))
-        {
-            DataManager.Instance.RetryGame();
-        }
-    }
+		remainingObjects = rigidbodys.Length;
+
+		Vector3 explosionCenter = transform.position + explosionOffset;
+
+		foreach (Rigidbody rigidbody in rigidbodys)
+		{
+			if (rigidbody == null) continue;
+
+			rigidbody.isKinematic = false;
+
+			rigidbody.useGravity = true;
+
+			rigidbody.AddExplosionForce(explosionForce, explosionCenter, explosionRadius);
+			Debug.Log("TruggerDeathEffects");
+
+			StartCoroutine(DisableObject(rigidbody.gameObject));
+		}
+
+	}
+
+	private IEnumerator DisableObject(GameObject obj)
+	{
+
+		Debug.Log("흠...");
+		yield return new WaitForSeconds(Random.Range(3, delay));
+		Debug.Log("없어졋는데..");
+		obj.SetActive(false);
+		remainingObjects--;
+		yield return null;
+		if (remainingObjects <= 0 && CompareTag("Frog"))
+		{
+
+			Debug.Log("RetryGame");
+			DataManager.Instance.RetryGame();
+		}
+	}
+
+
+
 }
