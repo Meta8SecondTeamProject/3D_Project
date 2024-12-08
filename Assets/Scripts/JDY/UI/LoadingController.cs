fb61e5289e0590464ba9f4b531da928c2c91695a
JDY_NOTE\jdy00 <sjm25148@gmail.com>
2024-12-08 12:51:15 +0900

    NPC들 대사창 다시 넣어줌

--- Assets/Scripts/JDY/UI/LoadingController.cs
+++ Assets/Scripts/JDY/UI/LoadingController.cs
@@ -1,49 +1,50 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class LoadingController : MonoBehaviour
 {
-	public Image progressBar;
-	[HideInInspector]
-	public string nextSceneName;
+    public Image progressBar;
+    [HideInInspector]
+    public string nextSceneName;
 
-	public void StartLoadingScene(string nextScene)
-	{
-		nextSceneName = nextScene;
-		StartCoroutine(LoadSceneProcess());
-	}
+    public void StartLoadingScene(string nextScene)
+    {
+        nextSceneName = nextScene;
+        StartCoroutine(LoadSceneProcess());
+    }
 
-	private IEnumerator LoadSceneProcess()
-	{
-		AsyncOperation operation = SceneManager.LoadSceneAsync(nextSceneName);
+    private IEnumerator LoadSceneProcess()
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(nextSceneName);
 
-		operation.allowSceneActivation = false;
-		float timer = 0f;
-		while (operation.isDone == false)
-		{
-			yield return null;
+        operation.allowSceneActivation = false;
+        float timer = 0f;
+        while (operation.isDone == false)
+        {
+            yield return null;
 
-			if (operation.progress < 0.9f)
-			{
-				progressBar.fillAmount = operation.progress;
-			}
-			else
-			{
-				timer += Time.unscaledDeltaTime;
-				progressBar.fillAmount = Mathf.Lerp(0.9f, 1f, timer);
+            if (operation.progress < 0.9f)
+            {
+                progressBar.fillAmount = operation.progress;
+            }
+            else
+            {
+                timer += Time.unscaledDeltaTime;
+                progressBar.fillAmount = Mathf.Lerp(0.9f, 1f, timer);
 
-				if (progressBar.fillAmount >= 1f)
-				{
-					operation.allowSceneActivation = true;
-					yield return null;
-					UIManager.Instance.ChangeScene();
-				}
+                if (progressBar.fillAmount >= 1f)
+                {
+                    operation.allowSceneActivation = true;
+                    yield return new WaitForSeconds(UnityEngine.Random.Range(0.5f, 2f));
+                    yield return new WaitUntil(() => operation.isDone);
+                    yield return null;
+                    UIManager.Instance.ChangeScene();
+                }
 
-			}
-		}
+            }
+        }
 
-	}
+    }
 }
