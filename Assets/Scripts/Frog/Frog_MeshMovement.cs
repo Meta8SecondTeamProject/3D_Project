5b2131086d69ae57f2463ff856f7e7cd390e319d
Chanyoung <qpalzm60515@gmail.com>
2024-12-11 20:51:11 +0900

    실험

--- Assets/Scripts/Frog/Frog_MeshMovement.cs
+++ Assets/Scripts/Frog/Frog_MeshMovement.cs
@@ -1,24 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class Frog_MeshMovement : MonoBehaviour
 {
-
-	public GameObject lookDir;
-	Rigidbody rb;
-
-	private void Awake()
-	{
-		rb = GetComponentInParent<Rigidbody>();
-	}
-
-	private void FixedUpdate()
-	{
-		//transform.rotation = Quaternion.Lerp(Quaternion.Euler(),);
-
-		//Vector3 dir = lookDir.transform.position - transform.position;
-		//transform.LookAt(dir);
-	}
-
 }
